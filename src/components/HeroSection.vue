0e2d8e15154cb4e6b1add444e779771c85397382
Javier Reyes <javier.reys@gmail.com>
2022-04-21 01:14:08 -0400

    feat: :lipstick: change the overall look and feel of the site

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="lg:relative">
+    <div
+      class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left"
+    >
+      <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
+        <h1
+          class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl"
+        >
+          <span class="block xl:inline"
+            >Organiza tu
+            <span class="text-green-600 xl:inline">alimentación</span> en un
+            solo lugar</span
+          >
+        </h1>
+        <p
+          class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl"
+        >
+          Nuestra misión es apoyar a las personas en su busqueda por una
+          alimentación más saludable y sustentable.
+        </p>
+        <div class="mt-10 sm:flex sm:justify-center lg:justify-start">
+          <div class="rounded-md shadow">
+            <a
+              href="#"
+              class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base rounded-md font-medium uppercase text-white bg-green-600 hover:bg-green-700 md:py-4 md:text-lg md:px-10"
+            >
+              Participar
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full"
+    >
+      <img
+        class="absolute inset-0 w-full h-full object-cover object-right"
+        src="../assets/food.jpg"
+        alt=""
+      />
+    </div>
+  </section>
+</template>
